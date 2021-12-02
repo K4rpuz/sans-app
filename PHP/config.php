f26b7992dd7654dfd84611952a5ffdbf16833d50
Esteban <esteban.naranjo.rayo@gmail.com>
2021-12-02 16:55:39 -0300

    archivos para conectar a la bd

---
+++ PHP/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$dbHost = 'localhost';
+$db = 'sansapp';
+$dbPort = '5432';
+$dbUser = 'postgres';
+$dbPass = '1234';
---
+++ PHP/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once config.php;
+
+function connect(string $host, string $db, string $dbPort, string $user, string $pass) {
+	try{
+		$dsn = "pgsql:host=$host;port=$dbPort;dbname=$db;";
+		return new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+	}catch(PDOException $e){
+		die("Connection failed: " . $e->getMessage());
+	}
+}
+
+return connect($host, $db, $dbPort, $user, $pass);
